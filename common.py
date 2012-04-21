369f1a511d1777d2111d1b9b80fec15e69bbdb2e
Rad <jan.rad@gmail.com>
2012-04-21 16:04:58 +0000

    move log to old folder

--- common.py
+++ common.py
@@ -3,23 +3,33 @@
 import logging
 import os
 from logging import handlers
-
-_log = None
+import shutil
+import atexit
+log = None
 
 MB = 1024 * 1024
 LOG_ROOT = '/var/log/newspack/'
 
-def get_logger():
-    global _log
-    if not _log:
+def mv_log(*log_fns):
+    for fn in log_fns:
+        if os.path.exists(fn):
+            shutil.move(fn, LOG_ROOT + '/old/')
+
+def getlogger():
+    global log
+    if not log:
         pid = os.getpid()
-        _log = logging.getLogger(__name__)
-        _log.setLevel(logging.DEBUG)
-        debug_fh = _init_handler(LOG_ROOT + str(pid) + '-news-debug.log')
-        fh = _init_handler(LOG_ROOT + str(pid) + '-news.log', logging.WARN)
-        _log.addHandler(debug_fh)
-        _log.addHandler(fh)
-    return _log        
+        debug_fn = LOG_ROOT + str(pid) + '-news-debug.log'
+        log_fn = LOG_ROOT + str(pid) + '-news.log'
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.DEBUG)
+        debug_fh = _init_handler(debug_fn)
+        fh = _init_handler(log_fn, logging.WARN)
+        log.addHandler(debug_fh)
+        log.addHandler(fh)
+        atexit.register(mv_log, debug_fn, log_fn) 
+        
+    return log        
 
 def _init_handler(fn, level=logging.DEBUG, maxBytes=30*MB, backupCount=10):
     fh = handlers.RotatingFileHandler(fn, maxBytes, backupCount) 
