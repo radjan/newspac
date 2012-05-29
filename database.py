ebdb7a3286c50af0765ee209b40523d951a9e2a7
Rad <jan.rad@gmail.com>
2012-05-29 14:20:06 +0000

    fix get_articles_amount_by_topics

--- database.py
+++ database.py
@@ -331,13 +331,13 @@ def get_articles_amount_by_topics(cursor, topics, start=None, end=None, limit=0)
                WHERE 
                    aid_table.amount = ?
                    AND t_a_r1.article_id = aid_table.aid
-                   AND t_a_r1.topic_title IN (%s)
-                   AND t_a_r2.topic_title NOT IN (%s)
+                   AND t_a_r1.topic_title = ?
              '''\
              + start_cause + end_cause + limit_cause
-        sql = sql % ((','.join(['?'] * len(topics)),) * 3)
+        sql = sql % (','.join(['?'] * len(topics)),)
+        print sql
         topics_tuple = tuple(topics)
-        cursor.execute(sql, topics_tuple + (len(topics_tuple),) + topics_tuple + topics_tuple)
+        cursor.execute(sql, topics_tuple + (len(topics_tuple), topics_tuple[0]))
     else:
         topic = topics[0]
         sql = '''
