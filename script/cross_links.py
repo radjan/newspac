ec3fce7f124ec91ad9efabfae3bf7ee9ac3e41f3
Rad <jan.rad@gmail.com>
2012-08-04 16:18:01 +0000

    if no new topic is created, process the new articles for cross link only

--- script/cross_links.py
+++ script/cross_links.py
@@ -4,18 +4,33 @@ sys.path.append(os.path.dirname(__file__) + '/..')
 import sqlite3
 import common
 import database as db
+import datetime
+TS_FORMAT = '%Y-%m-%d %H:%M:%S'
+ONE_DAY = datetime.timedelta(days=1)
+ONE_HOUR = datetime.timedelta(hours=1)
+#now the script runs every 3 hour, given it two chances
+PROCESS_WINDOW = ONE_HOUR * 7
 
 log = common.get_logger()
 
 conn = sqlite3.connect(common.DB_PATH)
 c = conn.cursor()
 
-topics = db.list_topics()
+latest_topic_sql = 'select max(created) from topic'
+c.execute(latest_topic_sql)
+latest = c.fetchone()[0]
+latest_dt = datetime.datetime.strptime(latest, TS_FORMAT)
+now = datetime.datetime.now()
 
 sql = 'select a.id, a.title, tar.brief, tar.topic_title'\
       ' from article as a join topic_article_rel as tar'\
-      ' on a.id = tar.article_id '
-c.execute(sql)
+      ' on a.id = tar.article_id'
+if (now - latest_dt) > PROCESS_WINDOW:
+    #no new topic, do the latest only
+    c.execute(sql + ' where a.created > ?', (now - PROCESS_WINDOW,))
+else:
+    c.execute(sql)
+
 fetch = c.fetchall()
 articles = {}
 for row in fetch:
@@ -27,6 +42,7 @@ for row in fetch:
 
 print len(articles)
 
+topics = db.list_topics()
 count = 0
 for t in topics:
     for aid, a in articles.items():
