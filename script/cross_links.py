67e9784edd479bc5ab47c2d73f2256b476ee43b6
Rad <jan.rad@gmail.com>
2012-08-29 17:27:05 +0000

    add cached for relation checking

--- script/cross_links.py
+++ script/cross_links.py
@@ -22,8 +22,8 @@ latest = c.fetchone()[0]
 latest_dt = datetime.datetime.strptime(latest, TS_FORMAT)
 now = datetime.datetime.now()
 
-sql = 'select a.id, a.title, tar.brief, tar.topic_title'\
-      ' from article as a join topic_article_rel as tar'\
+sql = 'select a.id, a.title, tar.brief, tar.topic_title, a.cached'\
+      ' from article as a left outer join topic_article_rel as tar'\
       ' on a.id = tar.article_id'
 if (now - latest_dt) > PROCESS_WINDOW:
     #no new topic, do the latest only
@@ -37,8 +37,10 @@ for row in fetch:
     a = articles.setdefault(row[0], {'topics': set()})
     a['id'] = row[0]
     a['title'] = unicode(row[1])
-    a['brief'] = unicode(row[2])
-    a['topics'].add(row[3])
+    a['brief'] = unicode(row[2] if row[2] else '')
+    if row[3]:
+        a['topics'].add(row[3])
+    a['cached'] = unicode(row[4] if row[4] else '')
 
 print len(articles)
 
@@ -47,8 +49,8 @@ count = 0
 for t in topics:
     for aid, a in articles.items():
         if t not in a['topics']:
-            if t.lower() in a['title'].lower() or t.lower() in a['brief'].lower():
-                db.insert_or_update_t_a_rel(t, aid, a['brief'])
+            if t.lower() in (a['title'] + a['brief'] + a['cached']).lower():
+                db.insert_or_update_t_a_rel(t, aid, a['brief'] if a['brief'] else a['cached'])
                 count += 1
 conn.commit()
 print 'add %s links' % count
