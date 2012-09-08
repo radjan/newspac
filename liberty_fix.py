7f109ebe017b7eae0e583d4938415e6fe51c5a1a
Rad <jan.rad@gmail.com>
2012-09-08 09:31:02 +0000

    parse iservice url

--- liberty_fix.py
+++ liberty_fix.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 import urllib2
+import urllib
 import database as db
 from script import liberty_parse as lp
 import time
@@ -8,22 +9,27 @@ def fix_0A(a):
     if '0A' in a['url']:
         print a['url']
         a['url'] = a['url'].replace('0A', '0')
-        fetch_text(a)
         return True
     return False
 
 def fetch_text(a):
+    if a['cached']:
+        return False
     time.sleep(0.2)
-    txt = lp.fetch_text(a['url'])
+    txt = lp.fetch_text(urllib.unquote(a['url']))
     if txt:
         print a['title'], a['url']
         a['cached'] = txt
         return True
     return False
 
+
 source = u'自由時報'
-articles = db.list_articles_by_source(source)
+articles = db.list_articles_by_source(source, addition_cols=['cached'])
+fixed = 0
 for a in articles:
-    if fix_0A(a):
+    if any((fix_0A(a), fetch_text(a))):
         #print 'fixed', a
         db.ensure_article_exists(a, overwrite=True)
+        fixed += 1
+print 'total: %s, fixed: %s' % (len(articles), fixed)
--- script/liberty_parse.py
+++ script/liberty_parse.py
@@ -6,7 +6,11 @@ def fetch_text(url):
     try:
         u = urllib2.urlopen(url)
         bs = BeautifulSoup(u)
-        return bs.find(attrs={'id': re.compile('newsContent')}).find_all('span')[1].get_text()
+        if 'iservice' in url:
+            return bs.find(attrs={'id': re.compile('newsc'),
+                                  'role': re.compile('article')}).get_text()
+        else:
+            return bs.find(attrs={'id': re.compile('newsContent')}).find_all('span')[1].get_text()
     except Exception, e:
         #raise
         return None
