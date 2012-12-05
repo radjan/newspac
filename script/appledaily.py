69f9a502ee0f0c40f5003e82056d3f6e9dc3ad8a
Rad <jan.rad@gmail.com>
2012-12-05 12:38:42 +0000

    put together

--- script/appledaily.py
+++ script/appledaily.py
@@ -1,5 +1,7 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
 from datetime import datetime
-import appledaily_parse
 import rss_base
 # Sat, 01 Dec 2012 07:00:00 +0800
 process_date = lambda x: x[5:-6]
@@ -7,9 +9,19 @@ DATE_FORMAT =  '%d %b %Y %H:%M:%S'
 
 class Handler(rss_base.RssBaseHandler):
     def fetch_text(self, link):
-        return appledaily_parse.fetch_text(link)
+        return fetch_text(link)
 
     def _str_to_ts(self, pubdate_str):
         ts = datetime.strptime(process_date(pubdate_str), DATE_FORMAT)
         ts = ts.replace(tzinfo=rss_base.UTC8())
         return ts.astimezone(rss_base.UTC())
+
+def fetch_text(url):
+    try:
+        u = urllib2.urlopen(url)
+        url = u.geturl()
+        bs = BeautifulSoup(u)
+        return url, bs.find(attrs={'class': re.compile('articulum')}).get_text()
+    except Exception, e:
+        #raise
+        return url, None
